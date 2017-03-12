ed91bedb68534fd14ea6fdfe8e55747c1878de5f
Dmitri <perepelenkodmitri@gmail.com>
2017-03-12 16:30:45 +0000

    add random service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,20 @@ import { SamplerService } from './services/sampler.service';
 import { MetronomeService } from './services/metronome.service';
 import { PatternService } from './services/pattern.service';
 import { RandomService } from './services/random.service';
+import { ApiService } from './services/api.service';
 import { PresetManagerService } from './services/preset-manager.service';
 import { SeqComponent } from './seq/seq.component';
 import { StepOptionsComponent } from './step-options/step-options.component';
 import { PresetManagerComponent } from './preset-manager/preset-manager.component';
+import { PreventDefaultDirective } from './directives/prevent-default.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     SeqComponent,
     StepOptionsComponent,
-    PresetManagerComponent
+    PresetManagerComponent,
+    PreventDefaultDirective
   ],
   imports: [
     BrowserModule,
@@ -33,6 +36,7 @@ import { PresetManagerComponent } from './preset-manager/preset-manager.componen
     PatternService,
     PresetManagerService,
     RandomService,
+    ApiService,
   ],
   bootstrap: [AppComponent]
 })
--- src/app/services/random.service.ts
+++ src/app/services/random.service.ts
@@ -6,23 +6,20 @@ export class RandomService {
   constructor() { }
 
   getRandomString(length) {
-    let result = '';
     const mask = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-    for (let i = length; i > 0; --i) {
-      result += mask[Math.floor(Math.random() * mask.length)];
-    };
-
-    return result;
+    return this.r(mask, length);
   }
 
   getRandomNumber(length) {
-    let result = '';
     const mask = '0123456789';
+    return this.r(mask, length);
+  }
 
+  r(mask, length) {
+    let result = '';
     for (let i = length; i > 0; --i) {
       result += mask[Math.floor(Math.random() * mask.length)];
-    };
+    }
 
     return result;
   }
