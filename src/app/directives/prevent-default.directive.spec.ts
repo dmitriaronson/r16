f9899d6670091d1be350b66fa45ddd20bdd4ae73
Dmitri <perepelenkodmitri@gmail.com>
2017-03-12 16:27:18 +0000

    add directive for preventing default event

---
+++ src/app/directives/prevent-default.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { PreventDefaultDirective } from './prevent-default.directive';
+
+describe('PreventDefaultDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PreventDefaultDirective();
+    expect(directive).toBeTruthy();
+  });
+});
