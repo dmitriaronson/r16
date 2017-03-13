5928a2a73be72e3a5b028453932e375bec7737eb
Dmitri <perepelenkodmitri@gmail.com>
2017-03-13 23:05:03 +0000

    cli updates dohh

--- src/samples/import.js
+++ src/samples/import.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const slug = require('slug');
-const filenames = fs.readdirSync('src/samples').filter(f => f.includes('.ogg'));
+const filenames = fs.readdirSync('src/samples').filter(f => f.includes('.mp3'));
 
 console.log(`Found ${filenames.length} samples`);
 
 const content = `
   export default [${filenames.map((filename, i) => {
-    const name = filename.replace('.ogg', '');
-    const slugName = `${slug(name.trim().toLowerCase())}.ogg`;
+    const name = filename.replace('.mp3', '');
+    const slugName = `${slug(name.trim().toLowerCase())}.mp3`;
 
     fs.renameSync(`./src/samples/${filename}`, `./src/samples/${slugName}`);
 
